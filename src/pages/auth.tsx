af7f0ae840f8195fdf90d8c4cf236c4d3769c2b9
oxcode <mrexcelsam1@live.com>
2025-05-13 23:17:10 +0100

    setup

---
+++ src/pages/auth.tsx
@@ -0,0 +1,11 @@
+const AuthPage = () => {
+    return (
+        <>
+            <div className="flex w-full items-center justify-center h-screen">
+                Hello Login
+            </div>
+        </>
+    )
+}
+
+export default AuthPage;
