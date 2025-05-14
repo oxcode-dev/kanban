daaeeb9bb630ffeb4ad27772390a009170f5193c
oxcode <mrexcelsam1@live.com>
2025-05-14 13:58:23 +0100

    setup

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -2,9 +2,9 @@ const AuthPage = () => {
     return (
         <>
             <div className="flex w-full justify-center h-screen bg-gray-200 py-8">
-                <div className="border border-red-600 max-w-xl p-4">
-                    <div>
-
+                <div className="border border-slate-200 shadow bg-white min-w-lg rounded-lg p-4">
+                    <div className="">
+                        <h2>Kanban</h2>
                     </div>
                 </div>
             </div>
