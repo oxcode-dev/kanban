df18386a00e275c7e1f549429c260226c4017730
oxcode <mrexcelsam1@live.com>
2025-05-15 23:20:54 +0100

    setup

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -5,12 +5,12 @@ const AuthPage = () => {
                 <div className="border border-slate-200 shadow bg-white min-w-lg rounded-lg p-4">
                     <div className="p-4 sm:px-8 flex flex-col">
                         <div className="pb-8 pt-4">
-                            <h2>Kanban</h2>
+                            <h2 className="font-bold text-3xl text-blue-600 drop-shadow">Kanban</h2>
                         </div>
 
                         {/* Login */}
                         <div>
-                            <div className="py-4 space-y-3">
+                            <div className="py-4 pb-6 space-y-1">
                                 <p className="text-base text-gray-500 font-medium">
                                     Please enter your details
                                 </p>
