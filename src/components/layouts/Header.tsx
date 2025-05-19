b9bca25f2ebe06603b21c097434ad1715ec929ae
oxcode <mrexcelsam1@live.com>
2025-05-19 23:05:21 +0100

    setup

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -5,7 +5,16 @@ const Header = () => {
                 <div className="flex mx-auto justify-between container">
                     <div>Kanban</div>
                     <div>
-                        Hello Nav
+                        <nav>
+                            <ul className="flex space-x-2 items-center">
+                                <li>
+                                    <a href="">notification</a>
+                                </li>
+                                <li>
+                                    <a href="">user</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
