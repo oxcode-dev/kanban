720f9e51b4c13af36852979f6457ce389e4e6383
oxcode <mrexcelsam1@live.com>
2025-05-24 17:33:01 +0100

    setup

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -39,7 +39,7 @@ const ProjectTasks = () => {
                         {/* <TaskList /> */}
 
                         <div>
-                            <div className="bg-gray-100 my-4 max-w-xs rounded">
+                            <div className="bg-gray-100 my-4 max-w-xs rounded p-1">
                                 <div className="inline-flex items-center justify-between w-full p-2">
                                     <p>
                                         Research
@@ -53,7 +53,7 @@ const ProjectTasks = () => {
                                 <div>
                                     <div className="p-2">
                                         {/* Card */}
-                                        <div className="bg-white p-2">
+                                        <div className="bg-white p-2 shadow border border-gray-50">
                                             <div className="inline-flex justify-between items-center w-full">
                                                 <p>
                                                     <span className="text-xs bg-gray-100 text-gray-600 inline-flex items-center space-x-1.5 p-0.5 px-1.5 rounded">
@@ -65,6 +65,15 @@ const ProjectTasks = () => {
                                                 <button className="cursor-pointer inline-flex justify-center items-center bg-white rounded p-1">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" /></svg>
                                                 </button>
+                                            </div>
+
+                                            <div className="py-1">
+                                                <p className="text-gray-500 font-semibold">
+                                                    Writing an Articles on AI Technology
+                                                </p>
+                                                <p className="text-gray-500 font-light text-sm truncate">
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam debitis animi qui cum eius suscipit aut neque! Suscipit, veritatis inventore. Excepturi illo facere rem corrupti neque blanditiis consectetur, nam culpa.
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
