cc2186175f73a3531b502133c8b8dd680430e480
oxcode <mrexcelsam1@live.com>
2025-05-24 17:27:23 +0100

    setup

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,4 +1,5 @@
-import BacklogList from "@/components/BacklogList";
+import BacklogList from "@/components/tasks/BacklogList";
+import TaskList from "@/components/tasks/TaskList";
 import DefaultLayout from "@/layout/default";
 
 const ProjectTasks = () => {
@@ -35,6 +36,42 @@ const ProjectTasks = () => {
 
                     <div>
                         {/* <BacklogList /> */}
+                        {/* <TaskList /> */}
+
+                        <div>
+                            <div className="bg-gray-100 my-4 max-w-xs rounded">
+                                <div className="inline-flex items-center justify-between w-full p-2">
+                                    <p>
+                                        Research
+                                    </p>
+
+                                    <button className="cursor-pointer inline-flex justify-center items-center bg-white rounded p-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+                                    </button>
+                                </div>
+
+                                <div>
+                                    <div className="p-2">
+                                        {/* Card */}
+                                        <div className="bg-white p-2">
+                                            <div className="inline-flex justify-between items-center w-full">
+                                                <p>
+                                                    <span className="text-xs bg-gray-100 text-gray-600 inline-flex items-center space-x-1.5 p-0.5 px-1.5 rounded">
+                                                        <span className="w-1.5 h-1.5 inline-flex bg-red-600 rounded-full"></span>
+                                                        <span className="text-xs font-semibold">On Track</span>
+                                                    </span>
+                                                </p>
+
+                                                <button className="cursor-pointer inline-flex justify-center items-center bg-white rounded p-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" /></svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                    
                 </div>
