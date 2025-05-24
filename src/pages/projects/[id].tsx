03d3b99290b8ccc19e6403c5f899cac3c058e552
oxcode <mrexcelsam1@live.com>
2025-05-24 15:10:42 +0100

    setup

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -8,20 +8,23 @@ const ProjectTasks = () => {
         <>
             <DefaultLayout>
                 <div className="mx-auto container">
-                    Hello World
-                
                     <h2>Tasks Board</h2>
 
-                    <div>
-                        {
-                            navItems.map((item, key) => (
-                                <div key={key}>
-                                    {item}
-                                </div>
-                            ))
-                        }
+                    <div className="border-b border-gray-200 py-4">
+                        <div className="flex justify-between">
+                            <div className="flex space-x-4">
+                                {
+                                    navItems.map((item, key) => (
+                                        <div key={key}>
+                                            {item}
+                                        </div>
+                                    ))
+                                }
 
+                            </div>
+                        </div>
                     </div>
+                   
                 </div>
             </DefaultLayout>
         </>
