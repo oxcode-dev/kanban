7bb1da0fdbc595a2fd91f0e08c8a398bb00a4e10
oxcode <mrexcelsam1@live.com>
2025-05-24 15:07:51 +0100

    setup

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,11 +1,27 @@
 import DefaultLayout from "@/layout/default";
 
 const ProjectTasks = () => {
+    const navItems = [
+        'Overview', 'Tasks', 'Backlogs', 'Comments'
+    ]
     return (
         <>
             <DefaultLayout>
                 <div className="mx-auto container">
                     Hello World
+                
+                    <h2>Tasks Board</h2>
+
+                    <div>
+                        {
+                            navItems.map((item, key) => (
+                                <div key={key}>
+                                    {item}
+                                </div>
+                            ))
+                        }
+
+                    </div>
                 </div>
             </DefaultLayout>
         </>
