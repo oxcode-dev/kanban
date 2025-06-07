f3978ad3fa5fe2531210d668f7444be6bbdc8d80
oxcode <mrexcelsam1@live.com>
2025-06-07 16:04:44 +0100

    setup

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -18,6 +18,7 @@ export const tasks: TaskProps[] = [
     { id: 101, title: "Write project proposal", status: 1 },
     { id: 102, title: "Design wireframes", status: 1 },
     { id: 103, title: "Create wireframes", status: 2 },
+    { id: 103, title: "Create Low wireframes", status: 2 },
     { id: 104, title: "Set up project repo", status: 2},
     { id: 105, title: "Research feature ideas", status: 3 },
     { id: 106, title: "Write user stories", status: 3 },
