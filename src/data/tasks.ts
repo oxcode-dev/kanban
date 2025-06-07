1e4e124beed99f9a57e0aa7ac43938c84f8ed2bf
oxcode <mrexcelsam1@live.com>
2025-06-07 15:47:10 +0100

    setup

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,20 +1,24 @@
-export const taskStatus = [
-    {
-      id: 1,
-      title: "To Do",
-      
-    },
-    {
-      id: 2,
-      title: "In Progress",
-    },
-    {
-      id: 3,
-      title: "Done",
-    }
+type TaskStatusProps = {
+    id: number, 
+    title: string,
+}
+export const taskStatus: TaskStatusProps[] = [
+    { id: 1, title: "To Do",},
+    { id: 2, title: "In Progress",},
+    { id: 3, title: "Done",}
 ];
 
-export const tasks = [
-    { id: 101, content: "Write project proposal" },
-    { id: 102, content: "Design wireframes" }
+type TaskProps = {
+    id: number, 
+    title: string,
+    status: number,
+}
+
+export const tasks: TaskProps[] = [
+    { id: 101, title: "Write project proposal", status: 1 },
+    { id: 102, title: "Design wireframes", status: 1 },
+    { id: 103, title: "Create wireframes", status: 2 },
+    { id: 104, title: "Set up project repo", status: 2},
+    { id: 105, title: "Research feature ideas", status: 3 },
+    { id: 106, title: "Write user stories", status: 3 },
 ]
