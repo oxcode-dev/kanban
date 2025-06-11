f16f72747aece71061edcaf7c857399b066a8fe9
oxcode <mrexcelsam1@live.com>
2025-06-11 09:32:22 +0100

    setup

---
+++ src/layout/auth.tsx
@@ -0,0 +1,24 @@
+import { JSX } from "react"
+
+type AuthLayoutProps = {
+    children: JSX.Element
+}
+const AuthLayout = ({ children } : AuthLayoutProps) => {
+    return (
+        <>
+            <div className="flex w-full justify-center h-screen bg-gray-200 py-8">
+                <div className="border border-slate-200 shadow bg-white min-w-lg rounded-lg p-4">
+                    <div className="p-4 sm:px-8 flex flex-col">
+                        <div className="pb-8 pt-4">
+                            <h2 className="font-bold text-3xl text-blue-600 drop-shadow">Kanban</h2>
+                        </div>
+
+                        <div>
+                            { children }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
